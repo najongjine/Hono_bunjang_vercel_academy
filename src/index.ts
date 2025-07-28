50bd57a070a67d53404e67d030d28342749aa830
najongjine <najongjin3@hotmail.com>
2025-07-28 11:24:52 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import dbtest from "./router/dbtest/dbtest.js";
 import auth from "./router/auth/auth.js";
 import memoRouter from "./router/memo/memo.js";
 import productRouter from "./router/product/product.js";
+import tosspayRouter from "./router/payment/tosspay.js";
 
 // app 이라는 객체를 만들어라. Hono 라는 클래스를 통해서
 // 클래스란건 어떻게 알음?? new 키워드 보고 눈치챔
@@ -42,6 +43,7 @@ app.route("/dbtest", dbtest);
 app.route("/api/auth", auth);
 app.route("/api/memo", memoRouter);
 app.route("/api/product", productRouter);
+app.route("/api/tosspay", tosspayRouter);
 
 /** 이건 코드가 드럽게 어렵게 생김
  * port:3000, console.log(`Server is running on http://localhost:${info.port}`);
